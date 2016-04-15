634d64eff1eb2d20c72a0033955080b6f8f4ea04
philanc <philanc@users.noreply.github.com>
2016-04-15 18:35:20 -0400

    cleaned tree

--- test/slappend.lua
+++ test/slappend.lua
@@ -1,18 +1,30 @@
 
 local ecode = [[
-print('hello'); h = 'hello!!!'
+-- slua emmbedded loader
 local asep="++sluapp++";asep=asep..asep
-local f = assert(io.open(arg[0], 'rb'))
+-- local f = assert(io.open(arg[0], 'rb'))
+local f = assert(io.open("/proc/self/exe", 'rb'))
 local s = f:read("*a"); f:close()
 local i,j=s:find(asep, 1, true)
-if i then local ac=s:sub(j+1); load(ac)()
+if i then 
+	local ac = s:sub(j+1); 
+	local f, msg = load(ac)
+	if f then f() 
+	else 
+		print("slua: cannot load appended code (maybe a syntax error)")
+	end
 end
 ]]
 
 local appcode = [[
-print("all this code has been appended. Yay!")
-print"Now, exiting..."
-os.exit(3)
+--appended code
+package.preload.myapp = function()
+ local m = { hello = function() print"hello, myapp!!!"; end }
+ return m
+end
+print("myapp has been appended. Yay!")
+--print"Now, exiting..."
+--os.exit(3)
 ]]
 
 local ecodesep = "++slua++"

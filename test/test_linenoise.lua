0f6ca1be32d25d10b7eb752e01c6cfb082bf89c7
philanc <philanc@users.noreply.github.com>
2016-04-21 21:42:05 -0400

    improved linenoise tty functions

---
+++ test/test_linenoise.lua
@@ -0,0 +1,22 @@
+ln = require'linenoise'
+
+mtcp = require'mtcp' -- for msleep()
+
+strf = string.format
+printf = function(...) print(strf(...)) end
+
+function t1()
+	curmode = ln.getmode() -- save current terminal mode
+	ln.setrawmode(1) -- opost=1 : keep output post-processing
+	i = 0
+	while not ln.kbhit() do
+		i = i + 1
+		print(i, "Press any key to stop")
+		mtcp.msleep(100) -- 100 millisec
+	end
+	ch = io.read(1)
+	print("restore mode", ln.setmode(curmode)) -- restore the terminal mode
+	printf("stop key: %q", ch)
+end
+
+t1()
